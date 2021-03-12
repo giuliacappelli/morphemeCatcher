0b1aaef2b0d0106a4d38bc48cc0d268e408f76fb
Giulia Cappelli <62989440+giuliacappelli@users.noreply.github.com>
2021-03-12 20:35:58 +0100

    Add files via upload

--- morphemeCatcher.py
+++ morphemeCatcher.py
@@ -1,29 +1,25 @@
-from gensim.test.utils import datapath
-from gensim import utils
-import gensim.models
-# from gensim.models import Word2Vec
-from gensim.models import KeyedVectors
-
 morflist = []
-with open("morfemi.txt") as morfile:
+with open("morphemes.txt") as morfile:
 	for line in morfile:
 		line = line.strip()
 		morflist.append(line)
 
 dictnouns = {}
-with open("nouns_itwac.txt") as infile:
+with open("input.txt") as infile:
 	for line in infile:
 		line = line.strip().split(",")
 		noun, freq = line
 		dictnouns[noun] = freq
 
 basemorf = ["a", "e", "o"]
-for noun in dictnouns:
-	for morf in morflist:
-		if noun.endswith(morf): # ?botto-bottone, *infrastrutta-infrastruttura, stanza-stanzone
-			base = noun[:-(len(morf))]
-			for el in basemorf:
-				maybe = base+el
-				# print(maybe)
-				if maybe in dictnouns:
-					print(noun, dictnouns[noun], maybe, dictnouns[maybe], "-"+morf)
+with open("output.txt", "w") as outfile:
+	for noun in dictnouns:
+		for morf in morflist:
+			if noun.endswith(morf): # ?botto-bottone, *infrastrutta-infrastruttura, stanza-stanzone
+				base = noun[:-(len(morf))]
+				for el in basemorf:
+					maybe = base+el
+					# print(maybe)
+					if maybe in dictnouns:
+						# print(noun, dictnouns[noun], maybe, dictnouns[maybe], "-"+morf)
+						outfile.write(noun+" "+dictnouns[noun]+" "+maybe+" "+dictnouns[maybe]+" "+"-"+morf+"\r\n")
